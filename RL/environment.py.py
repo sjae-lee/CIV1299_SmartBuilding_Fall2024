5a7546960349f7fac92f4d928dc32fc975c8c547
sjae.lee@utoronto.ca <sjae.lee@utoronto.ca>
2024-11-24 22:07:09 -0500

    RL

---
+++ RL/environment.py.py
@@ -0,0 +1,84 @@
+import numpy as np
+import gymnasium as gym
+
+class SimpleEnv(gym.Env):
+    def __init__(self,
+                 Ad, Bd_HVAC, Bd_dist,
+                 Cd, Dd_HVAC, Dd_dist,
+                 COP, Price, u_dist):
+        self._state = np.array([[24, 25.5]], dtype=np.float32)
+        self._current_indoor_temperature = np.array([self._state[0,0]], dtype=np.int32)
+        self._current_timestep = 0
+        self.Ad = Ad
+        self.Bd_HVAC = Bd_HVAC
+        self.Bd_dist = Bd_dist
+        self.Cd = Cd
+        self.Dd_HVAC = Dd_HVAC
+        self.Dd_dist = Dd_dist
+        self._COP = COP
+        self._Price = Price
+        self._u_dist = u_dist
+
+        self.observation_space = gym.spaces.Dict({"Tin": gym.spaces.Box(0, 50, dtype=float)})
+        self.action_space = gym.spaces.Box(-2, 0, dtype=float)
+
+    def _get_obs(self):
+        return {"Tin": self._current_indoor_temperature}
+    
+    def _get_info(self):
+        return {"curret_timestep": self._current_timestep}
+    
+    def reset(self):
+        super().reset()
+        self._state = np.array([[24, 25.5]], dtype=np.float32)
+        self._current_indoor_temperature = np.array([self._state[0,0]], dtype=np.int32)
+        self._current_timestep = 0
+        observation = self._get_obs()
+        info = self._get_info()
+        return observation, info
+
+    def compute_x(self, x0, u0_HVAC, u0_dist, 
+                Ad, Bd_HVAC, Bd_dist):
+        return np.dot(Ad,x0) + np.dot(Bd_HVAC,u0_HVAC) + np.dot(Bd_dist,u0_dist)
+
+    def compute_y(self, x0,u0_HVAC, u0_dist, 
+                Cd, Dd_HVAC, Dd_dist):
+        return np.dot(Cd,x0) + np.dot(Dd_HVAC,u0_HVAC) + np.dot(Dd_dist,u0_dist)
+
+    def step(self, action):
+        i = self._current_timestep
+        self._state = self.compute_x(self._state.T,
+                                np.array([[action*1000]]),
+                                self._u_dist[i:i+1,:].T,
+                                self.Ad, self.Bd_HVAC, self.Bd_dist).T
+        
+        self._current_indoor_temperature = self.compute_y(self._state.T,
+                                                     np.array([[action*1000]]),
+                                                     self._u_dist[i:i+1,:].T,
+                                                     self.Cd, self.Dd_HVAC, self.Dd_dist)[0]
+
+        reward = action/self._COP[i]/6 * self._Price[i]
+        self._current_timestep += 1
+        observation = self._get_obs()
+        info = self._get_info()
+        
+        return observation, reward, info
+
+# Electricity rates
+def func_price(i):
+    # $/kWh
+    hour = (i//6)%24
+    if hour<7:
+        return 0.082
+    elif (hour>=7)&(hour<11):
+        return 0.113
+    elif (hour>=11)&(hour<17):
+        return 0.17
+    elif (hour>=17)&(hour<19):
+        return 0.113
+    else:
+        return 0.082
+
+# HVAC COP
+def func_COP(To):
+    return -2/25 * To + 6
